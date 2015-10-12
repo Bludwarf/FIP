12fbdc6f6c2794d516f32c75164f6caf6f4d19bd
Bludwarf <bludwarf@gmail.com>
2015-10-12 10:32:47 +0200

    On passe par Raw Git pour charger un CSS Github

--- fip-grid.user.js
+++ fip-grid.user.js
@@ -22,11 +22,19 @@ function importJS(src) {
 }
 
 /**
- * Importe une feuille de style CSS dans le document
+ * Importe une feuille de style CSS dans le document.
+ * Attention : pour les fichiers RAW de Github utiliser les URL en rawgit. Cf : https://rawgit.com/ (remplacement automatisé dans cette fonction)
  * @param href lien vers la feuille de style
  * @author http://www.abeautifulsite.net/manipulating-stylesheets-with-greasemonkey/
  */
 function importCSS(href) {
+	// URL en RAW github
+	var rawGitRx = /\/\/raw\.githubusercontent\.com\//;
+	if (href.match(rawGitRx)) {
+		href = href.replace(rawGitRx, "//cdn.rawgit.com/");  // on utilise rawgit pour avoir le bon Content-Type
+		console.log('On passe par Raw Git pour charger la feuille de style => %s', href);
+	}
+
 	var link = window.document.createElement('link');
 	link.rel = 'stylesheet';
 	link.type = 'text/css';
